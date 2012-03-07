7e326cfeb34fbd97fe203c82dcfca46997cd9719
Jared Getzoff <jargetz@gmail.com>
2012-03-07 15:48:24 +0800

    change search radius to 10 km from 200m

--- lib/google_places/spot.rb
+++ lib/google_places/spot.rb
@@ -22,7 +22,7 @@ module GooglePlaces
       sensor = options.delete(:sensor) || false
       offset = options.delete(:offset) || ""
       location = Location.new(lat, lng)
-      radius = options.delete(:radius) || 200
+      radius = options.delete(:radius) || 10000
       language  = options.delete(:language)
       types = options.delete(:types)
 
